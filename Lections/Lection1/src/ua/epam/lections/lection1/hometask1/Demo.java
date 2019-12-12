c4e1fe1039256e55d495e8ffd124fde5ba99745e
Sergey Bychkov <appachey@gmail.com>
2019-12-12 18:46:38 +0200

    task 7

--- Lections/Lection1/src/ua/epam/lections/lection1/hometask1/Demo.java
+++ Lections/Lection1/src/ua/epam/lections/lection1/hometask1/Demo.java
@@ -4,13 +4,21 @@ public class Demo {
     public static void main(String[] args) {
         //Part6.main(new String[]{"1000"});
         //System.out.println(Math.pow(26, 0));
-        String str = "abc";
+        /*String str = "abc";
         int sum = 0;
         char[] strToChar = str.toUpperCase().toCharArray();
         for (int i = 0; i < strToChar.length; i++){
             sum += (charToInt(strToChar[i]) * Math.pow(26, strToChar.length - i - 1));
         }
-        System.out.println(sum);
+        System.out.println(sum);*/
+        int sum = 701;
+        for (int i = 0; sum > 0; i++){
+            sum /= 26;
+            System.out.println(( sum % 26));
+
+        }
+
+        //System.out.println((703 / 26));
     }
     public static int charToInt (char c){
         return (c - 'A') + 1;
