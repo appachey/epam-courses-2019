bd744a4b293e7084ce01d4d04ed0595bcb7faa20
Sergey Bychkov <appachey@gmail.com>
2019-12-13 12:57:15 +0200

    task 7 implements str to int method

--- Lections/Lection1/src/ua/epam/lections/lection1/hometask1/Demo.java
+++ Lections/Lection1/src/ua/epam/lections/lection1/hometask1/Demo.java
@@ -2,16 +2,16 @@ package ua.epam.lections.lection1.hometask1;
 
 public class Demo {
     public static void main(String[] args) {
-        //Part6.main(new String[]{"1000"});
-        //System.out.println(Math.pow(26, 0));
-        /*String str = "abc";
+        /*Part6.main(new String[]{"1000"});
+        System.out.println(Math.pow(26, 0));
+        String str = "abc";
         int sum = 0;
         char[] strToChar = str.toUpperCase().toCharArray();
         for (int i = 0; i < strToChar.length; i++){
             sum += (charToInt(strToChar[i]) * Math.pow(26, strToChar.length - i - 1));
         }
-        System.out.println(sum);*/
-        //int sum = 701;
+        System.out.println(sum);
+        int sum = 701;
         int num = 53;
         String output = "";
         while (num > 0){
@@ -25,10 +25,8 @@ public class Demo {
         }
         System.out.println(output);
 
-        //System.out.println((703 / 26));
-    }
-    public static int charToInt (char c){
-        return (c - 'A') + 1;
+        System.out.println((703 / 26));*/
+        System.out.println(Part7.chars2digits("ZZ"));
     }
     public static char intToChar (int num){
         return  (char) ((num + 'A') - 1);
--- Lections/Lection1/src/ua/epam/lections/lection1/hometask1/Part7.java
+++ Lections/Lection1/src/ua/epam/lections/lection1/hometask1/Part7.java
@@ -1,4 +1,15 @@
 package ua.epam.lections.lection1.hometask1;
 
 public class Part7 {
+    public static int chars2digits(String number){
+        int sum = 0;
+        char[] strToChar = number.toUpperCase().toCharArray();
+        for (int i = 0; i < strToChar.length; i++){
+            sum += (charToInt(strToChar[i]) * Math.pow(26, strToChar.length - i - 1));
+        }
+        return sum;
+    }
+    public static int charToInt (char c){
+        return (c - 'A') + 1;
+    }
 }
