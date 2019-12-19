2c677fee37d50339ce180a4feb4667e5a0057e57
Sergey Bychkov <appachey@gmail.com>
2019-12-19 17:47:38 +0200

    add constructor and fields to Matrix

--- Lections/Lection1/src/ua/epam/lections/lection2/hometask2/Matrix.java
+++ Lections/Lection1/src/ua/epam/lections/lection2/hometask2/Matrix.java
@@ -1,4 +1,13 @@
 package ua.epam.lections.lection2.hometask2;
 
 public class Matrix {
+    private double [][] matrix;
+    private int rows;
+    private int cols;
+
+    public Matrix(int rows, int cols) {
+        this.rows = rows;
+        this.cols = cols;
+        this.matrix = new double[this.rows][this.cols];
+    }
 }
