1809f6580c6c8f3e3014180d85a4595e755b4d45
Sergey Bychkov <appachey@gmail.com>
2019-12-20 15:10:25 +0200

    add isEqual method to Matrix

--- Lections/Lection1/src/ua/epam/lections/lection2/hometask2/Demo.java
+++ Lections/Lection1/src/ua/epam/lections/lection2/hometask2/Demo.java
@@ -2,8 +2,8 @@ package ua.epam.lections.lection2.hometask2;
 
 public class Demo {
     public static void main(String[] args) {
-        Circle circle1 = new Circle();
-        Circle circle2 = new Circle();
-        System.out.println(circle1.isPointBelongTo(0, 1));
+        Matrix matr1 = new Matrix(3, 2);
+        Matrix matr2 = new Matrix(3, 3);
+        System.out.println(Matrix.isEqual(matr1, matr2));
     }
 }
--- Lections/Lection1/src/ua/epam/lections/lection2/hometask2/Matrix.java
+++ Lections/Lection1/src/ua/epam/lections/lection2/hometask2/Matrix.java
@@ -3,11 +3,27 @@ package ua.epam.lections.lection2.hometask2;
 public class Matrix {
     private double [][] matrix;
     private int rows;
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getCols() {
+        return cols;
+    }
+
     private int cols;
 
     public Matrix(int rows, int cols) {
         this.rows = rows;
         this.cols = cols;
         this.matrix = new double[this.rows][this.cols];
+    }
+
+    public static boolean isEqual (Matrix mat1, Matrix mat2) {
+        if (mat1.getRows() == mat2.getRows() && mat1.getCols() == mat2.getCols()) {
+            return true;
+        }
+        return false;
     }
 }
