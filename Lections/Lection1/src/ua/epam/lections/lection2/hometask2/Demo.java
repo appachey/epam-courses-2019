bbfe4b3e17dfd9de636faf7b80493e271dc3f724
Sergey Bychkov <appachey@gmail.com>
2019-12-20 17:03:48 +0200

    add addMatrix method to Matrix

--- Lections/Lection1/src/ua/epam/lections/lection2/hometask2/Demo.java
+++ Lections/Lection1/src/ua/epam/lections/lection2/hometask2/Demo.java
@@ -2,8 +2,13 @@ package ua.epam.lections.lection2.hometask2;
 
 public class Demo {
     public static void main(String[] args) {
-        Matrix matr1 = new Matrix(3, 2);
+        Matrix matr1 = new Matrix(3, 3);
         Matrix matr2 = new Matrix(3, 3);
-        System.out.println(Matrix.isEqual(matr1, matr2));
+        matr1.printMatr();
+        System.out.println();
+        matr2.printMatr();
+        System.out.println();
+        Matrix addRes = Matrix.addMatrix(matr1, matr2);
+        addRes.printMatr();
     }
 }
--- Lections/Lection1/src/ua/epam/lections/lection2/hometask2/Matrix.java
+++ Lections/Lection1/src/ua/epam/lections/lection2/hometask2/Matrix.java
@@ -1,26 +1,77 @@
 package ua.epam.lections.lection2.hometask2;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Random;
+
 public class Matrix {
     private double [][] matrix;
-    private int rows;
+    private final int ROWS;
+    private final int COLS;
 
     public int getRows() {
-        return rows;
+        return ROWS;
     }
 
     public int getCols() {
-        return cols;
+        return COLS;
     }
 
-    private int cols;
+    public double[][] getMatrix() {
+        return this.matrix;
+    }
+
+    public void setMatrix(double[][] matrix) {
+        this.matrix = matrix;
+    }
 
     public Matrix(int rows, int cols) {
-        this.rows = rows;
-        this.cols = cols;
-        this.matrix = new double[this.rows][this.cols];
+        this.ROWS = rows;
+        this.COLS = cols;
+        this.matrix = new double[this.ROWS][this.COLS];
+        this.initMatr();
     }
 
-    public static boolean isEqual (Matrix mat1, Matrix mat2) {
+    private void initMatr() {
+        Random rand = new Random();
+        for (int i = 0; i < this.ROWS; i++) {
+            for (int j = 0; j < this.COLS; j++) {
+                this.matrix[i][j] = valGenerator();
+            }
+        }
+    }
+
+    public static Matrix addMatrix(Matrix mat1, Matrix mat2) {
+        double[][] matrix1 = mat1.getMatrix();
+        double[][] matrix2 = mat2.getMatrix();
+        double[][] result = new double[mat1.getRows()][mat1.getCols()];
+        Matrix resultMatr = new Matrix(mat1.getRows(), mat1.getCols());
+        for (int i = 0; i < mat1.getRows(); i++) {
+            for (int j = 0; j < mat1.getCols(); j++){
+                result[i][j] = matrix1[i][j] + matrix2[i][j];
+            }
+        }
+        resultMatr.setMatrix(result);
+        return resultMatr;
+    }
+
+    public void printMatr() {
+        for (int i = 0; i < this.ROWS; i++) {
+            for (int j = 0; j < this.COLS; j++){
+                System.out.printf("%7.2f", this.matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    private double valGenerator() {
+        Random rand = new Random();
+        BigDecimal bd = new BigDecimal(Double.toString(rand.nextDouble() * 100));
+        bd = bd.setScale(2, RoundingMode.HALF_UP);
+        return bd.doubleValue();
+    }
+
+    private static boolean isEqual (Matrix mat1, Matrix mat2) {
         if (mat1.getRows() == mat2.getRows() && mat1.getCols() == mat2.getCols()) {
             return true;
         }
