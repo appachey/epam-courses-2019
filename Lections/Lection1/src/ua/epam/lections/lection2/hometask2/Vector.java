3b1efc2fdf8746191f5741f78e896463df7dc7d2
appachey <appachey@gmail.com>
2019-12-23 17:28:15 +0200

    adding methods to Vector

--- Lections/Lection1/src/ua/epam/lections/lection2/hometask2/Vector.java
+++ Lections/Lection1/src/ua/epam/lections/lection2/hometask2/Vector.java
@@ -1,4 +1,31 @@
 package ua.epam.lections.lection2.hometask2;
 
 public class Vector {
+    private Object[] refs;
+    private int capacity;
+
+    public Vector() {
+        this.refs = new Object[10];
+        this.capacity = 0;
+    }
+
+    public void addRef(Object elem) {
+        if (this.capacity == refs.length) {
+            Object[] newRefs = new Object[refs.length + 10];
+            System.arraycopy(this.refs, 0, newRefs, 0, this.refs.length);
+            this.refs = newRefs;
+            this.refs[this.capacity] = elem;
+            this.capacity++;
+        } else {
+            this.refs[this.capacity] = elem;
+            this.capacity++;
+        }
+    }
+
+    public void clear() {
+        this.refs = new Object[10];
+        this.capacity = 0;
+    }
+
+
 }
