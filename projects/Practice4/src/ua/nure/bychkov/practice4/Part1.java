78074752d321105978f2e1f4aa621f93f2353101
Sergey Bychkov <appachey@gmail.com>
2020-01-28 12:29:37 +0200

    implements Part1 method

--- projects/Practice4/src/ua/nure/bychkov/practice4/Part1.java
+++ projects/Practice4/src/ua/nure/bychkov/practice4/Part1.java
@@ -1,12 +1,35 @@
 package ua.nure.bychkov.practice4;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Part1 {
     private static final String ENCODING = "cp1251";
     private static final String F_NAME = "part1.txt";
-    private static final String L_SEP = System.lineSeparator();
 
     public static void main(String[] args) {
-
+        String input = ReadFile.rFile(F_NAME, ENCODING);
+        System.out.println(invert(input));
     }
 
+    public static String invert(String input) {
+        StringBuilder sb = new StringBuilder(input);
+        String regex = "(?Um)\\b(\\w{4,})\\b";
+        Pattern p = Pattern.compile(regex);
+        Matcher match = p.matcher(sb);
+
+        while (match.find()) {
+            int count = match.group(1).length();
+            int position = match.start(1);
+            for (int i = 0; i < count; i++, position++) {
+                char c = match.group(1).charAt(i);
+                if (Character.isLowerCase(c)) {
+                    sb.setCharAt(position, Character.toUpperCase(c));
+                } else {
+                    sb.setCharAt(position, Character.toLowerCase(c));
+                }
+            }
+        }
+        return sb.toString();
+    }
 }
