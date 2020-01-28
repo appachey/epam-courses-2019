124f9d4a50486c1b149823fefe33ed9daffad41f
Sergey Bychkov <appachey@gmail.com>
2020-01-28 11:18:06 +0200

    add util for reding files

--- projects/Practice4/src/ua/nure/bychkov/practice4/Part1.java
+++ projects/Practice4/src/ua/nure/bychkov/practice4/Part1.java
@@ -1,6 +1,10 @@
 package ua.nure.bychkov.practice4;
 
 public class Part1 {
+    private static final String ENCODING = "cp1251";
+    private static final String F_NAME = "part1.txt";
+    private static final String L_SEP = System.lineSeparator();
+
     public static void main(String[] args) {
 
     }
---
+++ projects/Practice4/src/ua/nure/bychkov/practice4/ReadFile.java
@@ -0,0 +1,33 @@
+package ua.nure.bychkov.practice4;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+
+public class ReadFile {
+    private static final String ENCODING = "cp1251";
+    private static final String F_NAME = "part1.txt";
+    private static final String L_SEP = System.lineSeparator();
+
+    public static void main(String[] args) {
+        String input = ReadFile.rFile(F_NAME, ENCODING);
+        System.out.println(input);
+    }
+    public static String rFile(String fName, String encoding) {
+        StringBuilder output = new StringBuilder();
+        try {
+            BufferedReader bReader = new BufferedReader(new InputStreamReader(new FileInputStream(fName), encoding));
+            String str;
+            while ((str = bReader.readLine()) != null) {
+                output.append(str).append(L_SEP);
+            }
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        output.delete(output.lastIndexOf(L_SEP), output.lastIndexOf(L_SEP) + L_SEP.length());
+        return output.toString();
+    }
+}
