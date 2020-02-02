ff6b8d3a8ad99e0436c3428b8c3745c80c3bcd7a
appachey <appachey@gmail.com>
2020-02-02 15:21:30 +0200

    implements iterator for Part4

--- projects/Practice4/src/ua/nure/bychkov/practice4/Part4.java
+++ projects/Practice4/src/ua/nure/bychkov/practice4/Part4.java
@@ -1,10 +1,30 @@
 package ua.nure.bychkov.practice4;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Part4 implements Iterable<String>{
-    public static void main(String[] args) {
+    private String text;
+    private int textLength;
+    private Matcher match;
 
+    public Part4(String fileName, String encoding) {
+        text = ReadUtil.readFile(fileName, encoding);
+        textLength = text.length();
+        match = Pattern.compile("(?Us)\\S.+?[\\.\\?!]+").matcher(text);
+    }
+
+    public static void main(String[] args) {
+        Part4 part4 = new Part4("part4.txt", "cp1251");
+        Iterator it = part4.iterator();
+        System.out.println(it.next());
+        System.out.println(it.next());
+        System.out.println(it.next());
+        System.out.println(it.next());
+        System.out.println(it.next());
+        System.out.println(it.next());
     }
 
     @Override
@@ -13,19 +33,32 @@ public class Part4 implements Iterable<String>{
     }
 
     private class IteratorImpl implements Iterator<String> {
+        private int cursor;
+        private int lastReturned;
+
+        IteratorImpl() {
+            cursor = 0;
+            lastReturned = -1;
+        }
         @Override
         public boolean hasNext() {
-            return false;
+            return cursor != Part4.this.textLength;
         }
 
         @Override
         public String next() {
-            return null;
+            int start = cursor;
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            Part4.this.match.find(start);
+            cursor = Part4.this.match.end();
+            return Part4.this.match.group();
         }
 
         @Override
         public void remove() {
-
+                throw new UnsupportedOperationException();
         }
     }
 }
