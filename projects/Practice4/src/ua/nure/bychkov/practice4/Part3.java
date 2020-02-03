a1e2ee65ff64874d0707cffe9281e46646bc3ba6
Sergey Bychkov <appachey@gmail.com>
2020-02-03 12:40:12 +0200

    refactor Part3

--- projects/Practice4/src/ua/nure/bychkov/practice4/Part3.java
+++ projects/Practice4/src/ua/nure/bychkov/practice4/Part3.java
@@ -7,51 +7,39 @@ import java.util.regex.Pattern;
 public class Part3 {
     public static void main(String[] args) {
         parser("part3.txt", "cp1251");
-
     }
+
+    private static String finder(String base, String input) {
+        Matcher match;
+        if ("char".equals(base)) {
+            match = Pattern.compile("(?Um)\\b(\\p{Alpha})\\b").matcher(input);
+        } else if("String".equals(base)) {
+            match = Pattern.compile("(?Um)\\b(\\p{Alpha}{2,}+)\\b").matcher(input);
+        } else if("int".equals(base)) {
+            match = Pattern.compile("(?Um)(?<=\\s|^)(\\d+)(?=\\s|$)").matcher(input);
+        } else if("double".equals(base)) {
+            match = Pattern.compile("(?Um)(\\.\\d+|\\d+\\.\\d+|\\d+\\.)").matcher(input);
+        } else {
+            return "Incorrect input";
+        }
+        StringBuilder output = new StringBuilder();
+        if (!match.find()) {
+            return "No such values";
+        } else {
+            match.reset();
+            while(match.find()) {
+                output.append(match.group()).append(" ");
+            }
+        }
+        return output.toString().trim();
+    }
+
     public static void parser(String fileName, String encoding) {
         Scanner scan = new Scanner(System.in);
         String input = ReadUtil.readFile(fileName, encoding);
-        Matcher charMatch = Pattern.compile("(?Um)\\b(\\p{Alpha})\\b").matcher(input);
-        Matcher strMatch = Pattern.compile("(?Um)\\b(\\p{Alpha}{2,}+)\\b").matcher(input);
-        Matcher intMatch = Pattern.compile("(?Um)(?<=\\s|^)(\\d+)(?=\\s|$)").matcher(input);
-        Matcher doubleMatch = Pattern.compile("(?Um)(\\.\\d+|\\d+\\.\\d+|\\d+\\.)").matcher(input);
-        String consoleInput = "";
-        while (!"stop".equals(consoleInput)) {
-            consoleInput = scan.nextLine();
-            if ("char".equals(consoleInput)) {
-                StringBuilder charOutput = new StringBuilder();
-                while(charMatch.find()) {
-                    charOutput.append(charMatch.group(1)).append(" ");
-                }
-                charMatch.reset();
-                System.out.println(charOutput.toString().trim());
-            } else if("String".equals(consoleInput)) {
-                StringBuilder strOutput = new StringBuilder();
-                while (strMatch.find()) {
-                    strOutput.append(strMatch.group(1)).append(" ");
-                }
-                strMatch.reset();
-                System.out.println(strOutput.toString().trim());
-            } else if("int".equals(consoleInput)) {
-                StringBuilder intOutput = new StringBuilder();
-                while (intMatch.find()) {
-                    intOutput.append(intMatch.group(1)).append(" ");
-                }
-                intMatch.reset();
-                System.out.println(intOutput.toString().trim());
-            } else if("double".equals(consoleInput)) {
-                StringBuilder doubleOutput = new StringBuilder();
-                while (doubleMatch.find()) {
-                    doubleOutput.append(doubleMatch.group(1)).append(" ");
-                }
-                doubleMatch.reset();
-                System.out.println(doubleOutput.toString().trim());
-            } else {
-                if(!"stop".equals(consoleInput)) {
-                    System.out.println("No such values");
-                }
-            }
+        String consoleInput;
+        while (!"stop".equals(consoleInput = scan.nextLine())) {
+            System.out.println(finder(consoleInput, input));
         }
     }
 }
