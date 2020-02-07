033b9b8ed43c0aa0329de4aecb34595cfbea1b0c
Sergey Bychkov <appachey@gmail.com>
2020-02-07 15:21:40 +0200

    clear

--- Lections/Lection1/src/ua/epam/lections/lection1/hometask1/Demo.java
+++
@@ -1,9 +0,0 @@
-package ua.epam.lections.lection1.hometask1;
-
-public class Demo {
-    public static void main(String[] args) {
-        System.out.println(Part7.chars2digits("VA"));
-        System.out.println(Part7.digits2chars(573));
-        System.out.println(Part7.rightColumn("VT"));
-    }
-}
