552bc7991e3261d32e611662ce5d3b95369ef8b6
appachey <appachey@gmail.com>
2020-02-09 15:19:34 +0200

    add Part2 with "enter" emul

--- bychkov/Practice5/src/ua/nure/bychkov/practice5/MyInputStream.java
+++ bychkov/Practice5/src/ua/nure/bychkov/practice5/MyInputStream.java
@@ -4,13 +4,22 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public class MyInputStream extends InputStream {
+    private byte [] arr;
+    private int pos;
+    private int length;
+
+    public MyInputStream(byte[] arr) {
+        this.arr = arr;
+        pos = 0;
+        length = arr.length;
+    }
     @Override
     public int read() throws IOException {
         try {
-            Thread.sleep(2000);
+            Thread.sleep(1000);
         } catch (InterruptedException ex) {
             ex.printStackTrace();
         }
-        return -1;
+        return (pos < length) ? (arr[pos++] & 0xff) : -1;
     }
 }
--- bychkov/Practice5/src/ua/nure/bychkov/practice5/Part2.java
+++ bychkov/Practice5/src/ua/nure/bychkov/practice5/Part2.java
@@ -1,14 +1,18 @@
 package ua.nure.bychkov.practice5;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 
 public class Part2 {
     private static final InputStream STD_IN = System.in;
 
-    public static void main(String[] args) throws IOException {
-        InputStream inStream = new MyInputStream();
-        System.setIn(new ByteArrayInputStream(System.lineSeparator().getBytes()));
+    public static void main(String[] args) throws InterruptedException {
+        InputStream inStream = new MyInputStream(System.lineSeparator().getBytes());
+        System.setIn(inStream);
+        Thread t = new Thread(){
+            public void run() {Spam.main(null);}
+        };
+        t.start();
+        t.join();
+        System.setIn(STD_IN);
     }
 }
