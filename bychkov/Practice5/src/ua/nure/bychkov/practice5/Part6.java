96dca26de813925a5c914c04b8855a65b61a3008
Sergey Bychkov <appachey@gmail.com>
2020-02-12 12:36:37 +0200

    add Part6

---
+++ bychkov/Practice5/src/ua/nure/bychkov/practice5/Part6.java
@@ -0,0 +1,15 @@
+package ua.nure.bychkov.practice5;
+
+public class Part6 {
+    private static final Object M = new Object();
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new Thread() {
+            public void run() {
+                // place your code here
+            }
+        };
+
+        // place your code here
+    }
+}
