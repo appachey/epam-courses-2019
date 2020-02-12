03d24175cb923f9cf6547c1848a4ee555dcdc3e2
Sergey Bychkov <appachey@gmail.com>
2020-02-12 17:29:44 +0200

    implements main

--- bychkov/Practice5/src/ua/nure/bychkov/practice5/Demo.java
+++ bychkov/Practice5/src/ua/nure/bychkov/practice5/Demo.java
@@ -1,10 +1,11 @@
 package ua.nure.bychkov.practice5;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 
 public class Demo {
-    public static void main(String[] args) throws InterruptedException, FileNotFoundException, UnsupportedEncodingException {
+    public static void main(String[] args) throws InterruptedException, IOException {
         System.out.println("=========================== PART1");
         Part1.main(new String[]{});
         System.out.println("=========================== PART2");
@@ -13,5 +14,7 @@ public class Demo {
         Part3.main(new String[]{});
         System.out.println("=========================== PART4");
         Part4.main(new String[]{});
+        System.out.println("=========================== PART5");
+        Part5.main(new String[]{});
     }
 }
--- bychkov/Practice5/src/ua/nure/bychkov/practice5/Part6.java
+++ bychkov/Practice5/src/ua/nure/bychkov/practice5/Part6.java
@@ -6,10 +6,29 @@ public class Part6 {
     public static void main(String[] args) throws InterruptedException {
         Thread t = new Thread() {
             public void run() {
-                // place your code here
+                synchronized (M) {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            M.notifyAll();
+                            M.wait();
+                        } catch (InterruptedException ex) {
+                            interrupt();
+                        }
+                    }
+                }
             }
         };
-
-        // place your code here
+        t.start();
+        synchronized (M) {
+            M.wait(100);
+            M.notifyAll();
+            System.out.println(t.getState());
+            M.wait();
+            System.out.println(t.getState());
+            t.interrupt();
+            M.wait(100);
+            System.out.println(t.getState());
+        }
+        t.join();
     }
 }
