b3131a6fb35b0e517de9b74fc57b72170c106f60
Sergey <appachey@gmail.com>
2020-02-19 18:49:33 +0200

    Part2 add removing by iterator

--- bychkov/Practice6/src/ua/nure/bycgkov/practice6/Part2.java
+++ bychkov/Practice6/src/ua/nure/bycgkov/practice6/Part2.java
@@ -1,6 +1,7 @@
 package ua.nure.bycgkov.practice6;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -8,12 +9,18 @@ public class Part2 {
     private static final int N = 10000;
 
     public static void main(String[] args) {
-        List<Integer> arrList = new ArrayList<>();
-        List<Integer> linkList = new LinkedList<>();
-        fillList(arrList);
-        fillList(linkList);
-        System.out.println("ArrayList#Index: " + removeByIndex(arrList, 4) + " ms");
-        System.out.println("LinkedList#Index: " + removeByIndex(linkList, 4) + " ms");
+        List<Integer> arrListRm = new ArrayList<>();
+        List<Integer> linkListRm = new LinkedList<>();
+        List<Integer> arrListIt = new ArrayList<>();
+        List<Integer> linkListIt = new LinkedList<>();
+        fillList(arrListRm);
+        fillList(linkListRm);
+        fillList(arrListIt);
+        fillList(linkListIt);
+        System.out.println("ArrayList#Index: " + removeByIndex(arrListRm, 4) + " ms");
+        System.out.println("LinkedList#Index: " + removeByIndex(linkListRm, 4) + " ms");
+        System.out.println("ArrayList#Iterator: " + removeByIterator(arrListIt, 4) + " ms");
+        System.out.println("LinkedList#Iterator: " + removeByIterator(linkListIt, 4) + " ms");
     }
 
     public static void fillList(List<Integer> list) {
@@ -24,14 +31,35 @@ public class Part2 {
 
     public static long removeByIndex(List<Integer> list, int k) {
         long start = System.currentTimeMillis();
+        int deleteInd = 0;
         while (list.size() != 1) {
             for (int i = 0; i < list.size(); i++) {
-                if (i % k == 0) {
+                deleteInd++;
+                if (deleteInd == k) {
                     list.remove(i);
+                    deleteInd = 0;
+                    i--;
                 }
             }
         }
+        return System.currentTimeMillis() - start;
+    }
 
+    public static long removeByIterator(List<Integer> list, int k) {
+        long start = System.currentTimeMillis();
+        int deleteInd = 0;
+        while (list.size() != 1) {
+            Iterator<Integer> it = list.iterator();
+            for (int i = 0; i < list.size(); i++) {
+                deleteInd++;
+                it.next();
+                if ( deleteInd == k) {
+                    it.remove();
+                    deleteInd = 0;
+                    i--;
+                }
+            }
+        }
         return System.currentTimeMillis() - start;
     }
 }
