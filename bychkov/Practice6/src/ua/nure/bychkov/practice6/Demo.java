d533ea34c3581ee8283f25c90edc3ae0644c84c8
appachey <appachey@gmail.com>
2020-02-23 15:58:42 +0200

    add Demo

---
+++ bychkov/Practice6/src/ua/nure/bychkov/practice6/Demo.java
@@ -0,0 +1,16 @@
+package ua.nure.bychkov.practice6;
+
+import java.io.IOException;
+
+public class Demo {
+    public static void main(String[] args) throws IOException {
+        System.out.println("=========================== PART1");
+        Part1.main(new String[]{});
+        System.out.println("=========================== PART2");
+        Part2.main(new String[]{});
+        System.out.println("=========================== PART3");
+        Part3.main(new String[]{});
+        System.out.println("=========================== PART4");
+        Part4.main(new String[]{});
+    }
+}
--- bychkov/Practice6/src/ua/nure/bychkov/practice6/Part4.java
+++ bychkov/Practice6/src/ua/nure/bychkov/practice6/Part4.java
@@ -1,7 +1,5 @@
 package ua.nure.bychkov.practice6;
 
-import java.util.Iterator;
-
 public class Part4 {
     public static void main(String[] args) {
         Range r = new Range (3, 10);
