c78de7eb4cf29c3a9487466cf2060b31e6e3ef63
Sergey <appachey@gmail.com>
2020-02-25 10:39:00 +0200

    tree implements add

--- bychkov/Practice6/src/ua/nure/bychkov/practice6/Part5.java
+++ bychkov/Practice6/src/ua/nure/bychkov/practice6/Part5.java
@@ -3,9 +3,7 @@ package ua.nure.bychkov.practice6;
 public class Part5 {
     public static void main(String[] args) {
         Tree<Integer> tree = new Tree<>();
-        System.out.println(tree.add(10));
-        System.out.println(tree.add(11));
-        System.out.println(tree.add(11));
-        System.out.println(tree.add(11));
+        Integer[] arr = {2,5,6,1,3,4,2};
+        tree.add(arr);
     }
 }
--- bychkov/Practice6/src/ua/nure/bychkov/practice6/Tree.java
+++ bychkov/Practice6/src/ua/nure/bychkov/practice6/Tree.java
@@ -8,33 +8,42 @@ public class Tree <E extends Comparable<E>> {
     // в противном случае элемент попадает в контейнер и метод возвращает true
     // первый добавляемый элемент становится корнем дерева
     // автобалансировки в дереве нет
-    private boolean addElem(Node<E> element, E value) {
-        Node<E> currentStruct = root;
-        if (element == null) {
-            element = new Node<>();
-            element.element = value;
-            if (root == null) {
-                root = element;
-                return true;
-            }
-            root = currentStruct;
-            return true;
-        }
 
-        if (value.compareTo(element.element) < 0) {
-            return addElem(currentStruct.leftBranch, value);
-        }
-        if (value.compareTo(element.element) > 0) {
-            return addElem(currentStruct.rightBranch, value);
-        }
-        return false;
-    }
     public boolean add(E element) {
-       return addElem(root, element);
+        Node<E> node = new Node<>();
+        node.element = element;
+       if (root == null) {
+           root = node;
+           return true;
+       }
+       Node<E> curent = root;
+       Node<E> parent;
+       while (true) {
+           parent = curent;
+           if (element.compareTo(curent.element) < 0) {
+               curent = curent.leftBranch;
+               if (curent == null) {
+                   parent.leftBranch = node;
+                   return true;
+               }
+           } else if (element.compareTo(curent.element) > 0) {
+               curent = curent.rightBranch;
+               if (curent == null) {
+                   parent.rightBranch = node;
+                   return true;
+               }
+           } else {
+               return false;
+           }
+       }
     }
 
     // добавляет все элементы из массива в контейнер (вызов в цикле метода add, см. выше)
-    public void add(E[] elements) {}
+    public void add(E[] elements) {
+        for (E element : elements) {
+            add(element);
+        }
+    }
 
     // удаляет элемент из контейнера
     // если удаляемого элемента в контейнере нет, то возвращает false
