4b2c3a27a0443d20fa16372cd39de445ff7f51a1
appachey <appachey@gmail.com>
2020-03-17 23:23:29 +0200

    Add calc practice 9

---
+++ bychkov/Practice9/src/ua/nure/bychkov/practice9/CalcServlet.java
@@ -0,0 +1,32 @@
+package ua.nure.bychkov.practice9;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/calc")
+public class CalcServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String op = request.getParameter("op");
+        int firstArg = Integer.parseInt(request.getParameter("firstArg"));
+        int secondArg = Integer.parseInt(request.getParameter("secondArg"));
+        int res = 0;
+        switch (op) {
+            case "plus":
+                res = firstArg + secondArg;
+                break;
+            case "minus":
+                res = firstArg - secondArg;
+                break;
+            default:
+                break;
+        }
+        request.setAttribute("res", res);
+
+        request.getRequestDispatcher("result.jsp").forward(request, response);
+    }
+}
---
+++ bychkov/Practice9/web/result.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: sergey
+  Date: 17.03.2020
+  Time: 23:09
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Result</title>
+</head>
+<body>
+    <p>Result: ${requestScope.res}</p>
+    <a href="index.jsp">Get back to Canculator!</a>
+</body>
+</html>
